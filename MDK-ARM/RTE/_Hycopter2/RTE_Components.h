36b794444d85f273d2700b88e37768976b6b7be1
KHIEMDO <khiem@hes.sg>
2018-11-08 09:51:20 +0800

    First commit

---
+++ MDK-ARM/RTE/_Hycopter2/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Hycopter2' 
+ * Target:  'Hycopter2' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f3xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
